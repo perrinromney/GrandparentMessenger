cfc2e1d8a9f24556a8a1b5714df069588e1e6802
perrinromney <68754259+perrinromney@users.noreply.github.com>
2021-12-26 02:24:40 -0600

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,16 @@
+import * from "dial.js"
+var doc= document;
+var dial=doc.getElementById("dial");
+var people=doc.getElementById("people");
+var newbutton=doc.getElementById("new");
+var all=doc.getElementById("all");
+var uparrow=doc.getElementById("uparrow");
+var downarrow=doc.getElementById("downarrow");
+var screen=doc.getElementById("screen");
+var listen=doc.getElementById("listen");
+var record=doc.getElementById("record");
+var stop=doc.getElementById("stop");
+var send=doc.getElementById("send");
+
+
+console.log(dial.textContent + people.textContent + newbutton.textContent);
