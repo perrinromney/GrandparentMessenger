df6c7e207f3dd83ac8253343c6d60ebdc207e5e2
perrinromney <68754259+perrinromney@users.noreply.github.com>
2021-12-26 14:08:33 -0600

    button event listeners

--- src/hardware/simulation/script.js
+++ src/hardware/simulation/script.js
@@ -1,16 +1,49 @@
-import * from "dial.js"
+function peoplecallback(){
+    alert("PEOPLE Button Pressed");
+}
+function newcallback(){
+    alert("NEW Button Pressed");
+}
+function allcallback(){
+    alert("ALL Button Pressed");
+}
+function listencallback(){
+    alert("LISTEN Button Pressed");
+}
+function recordcallback(){
+    alert("RECORD Button Pressed");
+}
+function stopcallback(){
+    alert("STOP Button Pressed");
+}
+function sendcallback(){
+    alert("SEND Button Pressed");
+}
+
 var doc= document;
 var dial=doc.getElementById("dial");
-var people=doc.getElementById("people");
-var newbutton=doc.getElementById("new");
-var all=doc.getElementById("all");
+var peoplebutton=doc.getElementById("peoplebutton");
+//alert(peoplebutton.textContent);
+var newbutton=doc.getElementById("newbutton");
+//alert(newbutton.textContent);
+var allbutton=doc.getElementById("allbutton");
+//alert(allbutton.textContent);
+var listenbutton=doc.getElementById("listenbutton");
+//alert(listenbutton.textContent);
+var recordbutton=doc.getElementById("recordbutton");
+//alert(recordbutton.textContent);
+var stopbutton=doc.getElementById("stopbutton");
+//alert(stopbutton.textContent);
+var sendbutton=doc.getElementById("sendbutton");
+//alert(sendbutton.textContent);
 var uparrow=doc.getElementById("uparrow");
 var downarrow=doc.getElementById("downarrow");
 var screen=doc.getElementById("screen");
-var listen=doc.getElementById("listen");
-var record=doc.getElementById("record");
-var stop=doc.getElementById("stop");
-var send=doc.getElementById("send");
 
-
-console.log(dial.textContent + people.textContent + newbutton.textContent);
+peoplebutton.addEventListener("click",()=>peoplecallback());
+newbutton.addEventListener("click",()=>newcallback());
+allbutton.addEventListener("click",()=>allcallback());
+listenbutton.addEventListener("click",()=>listencallback());
+recordbutton.addEventListener("click",()=>recordcallback());
+stopbutton.addEventListener("click",()=>stopcallback());
+sendbutton.addEventListener("click",()=>sendcallback());
